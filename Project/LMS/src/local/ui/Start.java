6b6041aac7077ad094c871bf47e64c56c424ec86
Rinsutoringu <Chord_sky@outlook.com>
2025-04-30 22:16:52 +0800

    fix path bug

--- Project/LMS/src/local/ui/Start.java
+++
@@ -1,7 +0,0 @@
-package local.ui;
-
-import javax.swing.JFrame;
-
-public class Start {
-    
-}
---
+++ Project/LMS/src/test/TestUI.java
@@ -0,0 +1,13 @@
+package test;
+
+import javax.swing.JFrame;
+
+import local.ui.*;
+import local.ui.mainwindow.MainWindowUI;
+
+public class TestUI {
+    
+    public static void main(String[] args) {
+        new MainWindowUI();
+    }
+}
