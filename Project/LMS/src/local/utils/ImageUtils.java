ad4c87edc8805189c06b8d6cbf3f21dccf2503a0
Rinsutoringu <Chord_sky@outlook.com>
2025-05-05 16:52:33 +0800

    make function to process img

--- Project/LMS/src/local/utils/ImageUtils.java
+++ Project/LMS/src/local/utils/ImageUtils.java
@@ -12,7 +12,7 @@ public class ImageUtils {
     /**
      * 从文件载入图像
      * @param filePath 图像的相对路径
-     * @return 成功载入的图像对象
+     * @return 成功载入的图像对象 若载入失败则报错并返回null
      */
     public static Image loadImage(String filePath) {
         try {
@@ -23,4 +23,25 @@ public class ImageUtils {
             return null;
         }
     }
+
+
+    // 对载入的图像进行处理
+    public static BufferedImage toBufferedImage(Image img) {
+        
+        // 进行类型验证
+        if (img instanceof BufferedImage) {
+            return (BufferedImage) img;
+        };
+
+        BufferedImage bufferedImage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+    }
+
+    /**
+     * 将传入的图像处理为JLabel
+     * @param image 读取的图像对象
+     * @return 转换为JLable的图像
+     */
+    
+    
 }
